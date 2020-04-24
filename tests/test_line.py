b221bba1ffd8397255788abb0176bcfc220460ff
hoge <hoge@foo,bar>
2020-04-25 00:03:41 +0900

    testing line class.

--- tests/test_line.py
+++ tests/test_line.py
@@ -10,20 +10,26 @@ class TestLine(object):
     def motion(self):
         return Line(self.LINE_TOKEN)
 
-    @pytest.mark.parametrize(("code", "expected"), [(200, True), (400, False)])
-    def test_notify(self, motion, mocker, code, expected):
+    @pytest.mark.parametrize(
+        ("code", "filename", "expected"),
+        [(200, None, True), (200, "image.jpeg", True), (400, None, False)],
+    )
+    def test_notify(self, motion, mocker, code, filename, expected):
         resMock = mocker.Mock()
         resMock.status_code = code
         mocked_post = mocker.patch("requests.post")
         mocked_post.return_value = resMock
 
         message = "detected!"
-        filename = None
+        if filename is not None:
+            mocked_open = mocker.patch("builtins.open")
+            mocked_open.return_value = "file_data"
+
         ret = motion.notify(message, filename)
         assert ret is expected
         mocked_post.assert_called_once_with(
             self.LINE_API,
             data={"message": message},
             headers={"Authorization": "Bearer " + self.LINE_TOKEN},
-            files=None,
+            files=None if filename is None else {"imageFile": "file_data"},
         )
