ba3cd28b2432d7940b81df77ed7c9de1469fac5d
maitohchan <57651739+maitohchan@users.noreply.github.com>
2020-04-29 23:06:51 +0900

    implemented alarm.

--- pywatching/googlehome.py
+++ pywatching/googlehome.py
@@ -1,7 +1,7 @@
 import pychromecast
 
 
-class GoogleHomeMini(object):
+class GoogleHome(object):
     def __init__(self, name: str = None, ip_addr: str = None):
         if ip_addr:
             cast = pychromecast.Chromecast(str(ip_addr))
@@ -12,7 +12,7 @@ class GoogleHomeMini(object):
             cast = None
         self.__cast = cast
 
-    def play(self, mp3_rl):
+    def play(self, mp3_rl: str):
         if self.__cast is None:
             return
         self.__cast.wait()
