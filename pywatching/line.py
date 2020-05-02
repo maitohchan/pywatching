c4d7972cc8c70bca576941af454899dbb7fa6e12
maitohchan <57651739+maitohchan@users.noreply.github.com>
2020-05-02 22:22:43 +0900

    implemented logger.

--- pywatching/line.py
+++ pywatching/line.py
@@ -31,16 +31,3 @@ class Line(object):
         files = {"imageFile": open(filename, "rb")} if filename else None
         res = requests.post(self.API, data=payload, headers=headers, files=files)
         return res.status_code == 200
-
-
-"""
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("token", help="LINE token")
-    args = parser.parse_args()
-
-    line = Line(args.token)
-    line.notify(message="test")
-"""
